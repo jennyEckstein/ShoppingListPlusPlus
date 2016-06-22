7786aaa0d132c6444314624e5b64ac7cbc8ef6c8
Jenny <ycharnetskaya@gmail.com>
2016-06-21 20:52:22 -0400

    feat: added ability to edit shopping list

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeListDetails/EditListNameDialogFragment.java
@@ -0,0 +1,78 @@
+package com.udacity.firebase.shoppinglistplusplus.ui.activeListDetails;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.ServerValue;
+import com.udacity.firebase.shoppinglistplusplus.R;
+import com.udacity.firebase.shoppinglistplusplus.model.ShoppingList;
+import com.udacity.firebase.shoppinglistplusplus.utils.Constants;
+
+import java.util.HashMap;
+
+/**
+ * Lets user edit the list name for all copies of the current list
+ */
+public class EditListNameDialogFragment extends EditListDialogFragment {
+    private static final String LOG_TAG = ActiveListDetailsActivity.class.getSimpleName();
+    String mListName;
+
+
+    /**
+     * Public static constructor that creates fragment and passes a bundle with data into it when adapter is created
+     */
+    public static EditListNameDialogFragment newInstance(ShoppingList shoppingList) {
+        EditListNameDialogFragment editListNameDialogFragment = new EditListNameDialogFragment();
+        Bundle bundle = EditListDialogFragment.newInstanceHelper(shoppingList, R.layout.dialog_edit_list);
+        bundle.putString(Constants.KEY_LIST_NAME, shoppingList.getListName());
+        editListNameDialogFragment.setArguments(bundle);
+        return editListNameDialogFragment;
+    }
+
+    /**
+     * Initialize instance variables with data from bundle
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mListName = getArguments().getString(Constants.KEY_LIST_NAME);
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        /** {@link EditListDialogFragment#createDialogHelper(int)} is a
+         * superclass method that creates the dialog
+         **/
+        Dialog dialog = super.createDialogHelper(R.string.positive_button_edit_item);
+        helpSetDefaultValueEditText(mListName);
+
+        return dialog;
+    }
+
+    /**
+     * Changes the list name in all copies of the current list
+     */
+    protected void doListEdit() {
+        final String inputListName = mEditTextForList.getText().toString();
+
+        if(!inputListName.equals("")){
+            if(mListName != null){
+                if(!inputListName.equals(mListName)){
+                    Firebase shoppingListRef = new Firebase(Constants.FIREBASE_URL_ACTIVE_LIST);
+                    HashMap<String, Object> updatedProperties = new HashMap<>();
+                    updatedProperties.put(Constants.FIREBASE_PROPERTY_LIST_NAME, inputListName);
+
+                    HashMap<String, Object> changedTimestampMap = new HashMap<>();
+                    changedTimestampMap.put(Constants.FIREBASE_PROPERTY_TIMESTAMP, ServerValue.TIMESTAMP);
+
+                    updatedProperties.put(Constants.FIREBASE_PROPERTY_TIMESTAMP_LAST_CHANGED, changedTimestampMap);
+                    shoppingListRef.updateChildren(updatedProperties);
+                }
+            }
+        }
+    }
+}
+
